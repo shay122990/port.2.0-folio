43b8fbca3ec02abdbb2df67f83ab3df82b0e2834
Shay.Asanova <shay.asanova@gmail.com>
2023-05-29 15:25:27 +0400

    added a loader to the page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 import "./index.css";
+import { useState, useEffect } from "react";
 import { About } from "./components/about/about.component";
 import { Arrow } from "./components/arrow/arrow.component";
 import { Contact } from "./components/contact/contact.component";
-import Divider from "./components/divider/divider.component";
+import { Divider } from "./components/divider/divider.component";
 import { Footer } from "./components/footer/footer.components";
 import { Header } from "./components/header/header.component";
 import { NavBar } from "./components/navigation/nav.component";
 import { Projects } from "./components/projects/projects.component";
 import { Services } from "./components/services&tech/services&tech.component";
 import ThemeContextProvider from "./ThemeContext";
+import Loader from "./components/loader/loader.component";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <ThemeContextProvider>
       <div className='App'>
-        <NavBar />
-        <Header />
-        <Arrow />
-        <Divider text='About Me' id='about' />
-        <About />
-        <Divider text='Projects' id='projects' />
-        <Projects />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div>
+            <NavBar />
+            <Header />
+            <Arrow />
+            <Divider text='About Me' id='about' />
+            <About />
+            <Divider text='Projects' id='projects' />
+            <Projects />
 
-        <Divider text='Services & Tech' id='services' />
-        <Services />
-        <Divider text='Get in touch' id='contact' />
-        <Contact />
-        <Footer />
+            <Divider text='Services & Tech' id='services' />
+            <Services />
+            <Divider text='Get in touch' id='contact' />
+            <Contact />
+            <Footer />
+          </div>
+        )}
       </div>
     </ThemeContextProvider>
   );
---
+++ src/components/loader/loader.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loader.styles.css";
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <span className='loader-spinner'></span>
+    </div>
+  );
+};
+
+export default Loader;
