1d36fdaba0b857dcc5190f8202bcb650b5ab870e
Shay <moeshakasanova@gmail.com>
2024-03-25 14:48:08 +0400

    about,services edits, removed services

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -7,17 +7,17 @@ export const About = () => {
     <div className='about-container'>
       <img className='about-img' src={imageAbout} alt='' />
       <div className='about-info'>
-        <p>
+      <p>
           Hi there 👋🏼, I'm Shay..
           <br />
           I have always been captivated by the intersection of technology and
           design, which is why I pursued a career in frontend development. With
-          a deep understanding of frontend technologies like SCSS, JavaScript,
+          a deep understanding of frontend technologies like CSS, JavaScript,
           Reactjs, Figma, Wordpress and Firebase, I possess a strong foundation
           to bring your vision to life, where captivating design meets flawless
-          functionality.
-          <br />I am always eager to learn new technologies and techniques,
-          staying on top of the latest trends of development and design to
+          functionality, speed and compatibility.
+          <br />I am always eager to learn new tool,s and adapt to new environments.
+          Staying on top of the latest trends of development and design to
           ensure I can deliver the best possible results for your company as
           well as personal projects.
         </p>
