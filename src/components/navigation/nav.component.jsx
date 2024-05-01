a92284aebccc2730936c390fa7f74890e262ba45
Shay <moeshakasanova@gmail.com>
2024-05-01 16:08:24 +0400

    restructured navbar, mobile view

--- src/components/navigation/nav.component.jsx
+++ src/components/navigation/nav.component.jsx
@@ -1,39 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./nav.styles.css";
-import logo from "../../media/logo.gif";
 import { DarkModeToggle } from "../dark-mode-toggle/dark-mode-toggle";
 import { ThemeContext } from "../../ThemeContext";
 
 export const NavBar = () => {
   const { darkMode } = useContext(ThemeContext);
-  const links = [
-    { text: "Projects", href: "#projects" },
-    { text: "Services", href: "#services" },
-    {
-      text: "",
-      href: "https://github.com/shay122990",
-      icon: "fa-brands fa-github nav-icon",
-      target: "_blank",
-      rel: "noreferrer",
-    },
-    { text: "Home", href: "#home", logo: true },
-    { text: "About", href: "#about" },
-    { text: "Contact", href: "#contact" },
-  ];
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
   return (
     <nav className={`nav ${darkMode ? "dark" : ""}`}>
-      {links.map((link, index) => (
-        <a
-          href={link.href}
-          key={index}
-          target={link.target}
-          rel={link.rel}
-          className={`anchor ${darkMode ? "dark" : ""}`}
-        >
-          {link.icon ? <i className={link.icon}></i> : null}
-          {link.logo ? <img src={logo} alt="Logo" /> : link.text}
-        </a>
-      ))}
+      <button className="mobile-menu-icon" onClick={toggleMenu}>
+        <i
+          className={`fa-solid ${
+            mobileMenu ? "fa-xmark" : "fa-bars"
+          } menu-icon`}
+        />
+      </button>
+      <ul
+        className={mobileMenu ? "nav-links-mobile" : "nav-links"}
+        onClick={() => setMobileMenu(false)}
+      >
+        <li>
+          <a href="#about" onClick={toggleMenu}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#projects" onClick={toggleMenu}>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#services" onClick={toggleMenu}>
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#contact" onClick={toggleMenu}>
+            Contact
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/shay122990"
+            target="_blank"
+            rel="noreferrer"
+            onClick={toggleMenu}
+          >
+            <i className="fa-brands fa-github nav-icon"></i>
+          </a>
+        </li>
+      </ul>
       <DarkModeToggle />
     </nav>
   );
