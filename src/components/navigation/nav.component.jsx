906238746c97dc47cf5110fa3eafea7bf996b452
Shay <moeshakasanova@gmail.com>
2024-08-09 18:09:10 +0400

    mobile first styling navbar

--- src/components/navigation/nav.component.jsx
+++ src/components/navigation/nav.component.jsx
@@ -39,8 +39,8 @@ export const NavBar = () => {
           </a>
         </li>
         <li>
-          <a href="#tech" onClick={toggleMenu}>
-            Tech Stack
+          <a href="#services" onClick={toggleMenu}>
+            Services
           </a>
         </li>
         <li>
